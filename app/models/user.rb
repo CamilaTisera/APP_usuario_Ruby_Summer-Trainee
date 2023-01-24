1eb2738129d3eba3b265b4328bf8999f5aecba32
CamilaTisera <camilanoemitisera@gmail.com>
2023-01-24 16:38:34 -0300

    Create controller and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,30 @@
 class User < ApplicationRecord
   # Associations
-  has_many :comments
+  #has_many :comments
+  
+   # Will return an array of follows for the given user instance
+   has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+
+   # Will return an array of users who follow the user instance
+   has_many :followers, through: :received_follows, source: :follower
+   
+   #####################
+   
+   # returns an array of follows a user gave to someone else
+   has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+   
+   # returns an array of other users who the user has followed
+   has_many :followings, through: :given_follows, source: :followed_user
+
 
   # Validations
-  validates :name, presence: true, uniqueness: true
+  #validates :name, presence: true, uniqueness: true
 
   # Enumerations
-  enum currency: [:ars, :usd, :eur] # Revisar buenas prácticas
+  #enum currency: [:ars, :usd, :eur] # Revisar buenas prácticas
 
   # Methods
-  def free?
-    price.zero?
-  end
+  #def free?
+   # price.zero?
+  #end
 end
--- test/models/user_1_test.rb
+++ test/models/user_1_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CommentTest < ActiveSupport::TestCase
+class User1Test < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
